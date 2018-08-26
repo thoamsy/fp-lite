c02421345ed42e54b7bf96d085b5317e677432f2
YangKui <c6swift@gmail.com>
2018-08-26 12:40:19 +0800

    refactor: 使用递归重写 flatten

--- src/flatten.js
+++ src/flatten.js
@@ -1,3 +1,8 @@
-export default function flatten(arry = []) {
-  return arry.reduce((result, step) => result.concat(step), []);
+'use strict';
+
+export default function flatten(array = [], result = []) {
+  return array.length
+    ? flatten(array.slice(1), result.concat(array[0]))
+    : result;
+  // return array.reduce((result, step) => result.concat(step), []);
 }
