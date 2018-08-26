d713de9c72d7342f8c8b09d0019e8ee9bdc740ca
YangKui <c6swift@gmail.com>
2018-08-26 17:49:42 +0800

    config: prettier 的 script

--- src/flatten.js
+++ src/flatten.js
@@ -1,8 +1,3 @@
-'use strict';
-
 export default function flatten(array = [], result = []) {
-  return array.length
-    ? flatten(array.slice(1), result.concat(array[0]))
-    : result;
-  // return array.reduce((result, step) => result.concat(step), []);
+  return array.length ? flatten(array.slice(1), result.concat(array[0])) : result;
 }
--- test/map.test.js
+++ test/map.test.js
@@ -12,11 +12,7 @@ describe('the function should be same like the Array.prototype.map', () => {
 
   it('should pluck the property for objects', () => {
     const pluckWithName = map(prop('name'));
-    const objects = [
-      { name: 'a', age: 18 },
-      { name: 'b', age: 20 },
-      { name: 'c', age: 39 },
-    ];
+    const objects = [{ name: 'a', age: 18 }, { name: 'b', age: 20 }, { name: 'c', age: 39 }];
     const result = pluckWithName(objects);
     expect(result).toHaveLength(3);
     expect(result).toContain('b');
