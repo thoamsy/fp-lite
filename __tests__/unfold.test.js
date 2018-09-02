c8353249e29056fec067c5ab67193a7ffd4c0838
YangKui <c6swift@gmail.com>
2018-09-02 14:39:45 +0800

    test: unfold's test

---
+++ __tests__/unfold.test.js
@@ -0,0 +1,13 @@
+import unfold from '../src/unfold';
+
+describe('test my unfold function', () => {
+  it('should can generate the array', () => {
+    const f = n => (n > 50 ? false : [-n, n + 10]);
+    expect(unfold(f, 10)).toEqual([-10, -20, -30, -40, -50]);
+  });
+
+  it('should can unfold the 2d array', () => {
+    const f = n => (n.length >= 5 ? false : [n, n.concat(1)]);
+    expect(unfold(f, [])).toEqual([[], [1], [1, 1], [1, 1, 1], [1, 1, 1, 1]]);
+  });
+});
